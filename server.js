3a68593037a0e9cd3a853d5b2f297ce3e6d2da55
dishanfernando91 <dmfernando91@gmail.com>
2021-01-03 12:38:36 +0530

    Sorting and bug fixes

--- server.js
+++ server.js
@@ -5,30 +5,30 @@ const app = express()
 
 connectDB()
 
-// function auth (req, res, next) {
-//     console.log(req.headers);
-//     var authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         var err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');
-//         err.status = 401;
-//         next(err);
-//         return;
-//     }
-//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//     var user = auth[0];
-//     var pass = auth[1];
-//     if (user == 'admin' && pass == 'password') {
-//         next(); // authorized
-//     } else {
-//         var err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');      
-//         err.status = 401;
-//         next(err);
-//     }
-//   }
+function auth (req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'Mangala' && pass == 'Divulapitiya') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');      
+        err.status = 401;
+        next(err);
+    }
+  }
   
-// app.use(auth);
+app.use(auth);
 
 //Init Middleware
 app.use(express.json({ extended: false }))
